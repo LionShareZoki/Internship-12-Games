e176a17a425d477bdfc0cea9d96a02316ee9b7dd
LionShareZoki <pujic.zoran@gmail.com>
2024-02-19 20:49:51 +0100

    add method to fetchGameStoresById

--- helpers/getGames.js
+++ helpers/getGames.js
@@ -59,3 +59,14 @@ export const fetchGameStores = (gameId, callback) => {
 };
 
 
+export const fetchGameStoresById = (gameId, callback) => {
+  const baseUrl = `https://api.rawg.io/api/games/${gameId}/stores`;
+  const url = new URL(baseUrl);
+  url.search = new URLSearchParams({ key: apiKey }).toString();
+
+  fetch(url)
+      .then(response => response.json())
+      .then(data => callback(data.results))
+      .catch(error => console.error('Error fetching stores: ', error));
+};
+
