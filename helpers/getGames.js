2171e1b63307923e9c300d1656e3b0bbad83f02f
LionShareZoki <pujic.zoran@gmail.com>
2024-02-19 20:48:42 +0100

    craete method to fetch games

--- helpers/getGames.js
+++ helpers/getGames.js
@@ -1,1 +1,13 @@
 const apiKey = '464bc085dbbf4f33bcb2ccb39d36a6ec';
+
+
+export const fetchGames = (params, callback) => {
+  const baseUrl = 'https://api.rawg.io/api/games';
+  const url = new URL(baseUrl);
+  url.search = new URLSearchParams({ ...params, key: apiKey }).toString();
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => callback(data.results))
+    .catch(error => console.error('Error fetching data: ', error));
+};
